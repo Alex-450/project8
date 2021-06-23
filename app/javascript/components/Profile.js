e0b567ec321fb5c7de82f7ce93dd98e1c5710f3e
AlexStearn <53116798+Alex-450@users.noreply.github.com>
2021-06-23 15:53:46 +0200

    Profile styling

--- app/javascript/components/Profile.js
+++ app/javascript/components/Profile.js
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
 import { UserContext } from "./Context/UserContext";
+import {Container, Jumbotron} from "react-bootstrap"
 
 function Profile() {
   const { userData } = useContext(UserContext);
   return (
-    <div>
-      <p>{userData.id}</p>
-      <p>{userData.first_name}</p>
-      <p>{userData.last_name}</p>
-      <p>{userData.email}</p>
-    </div>
+    <Container fluid className="form_container">
+      <Jumbotron className="form_jumbotron">
+      <p>First name: {userData.first_name}</p>
+      <p>Last name: {userData.last_name}</p>
+      <p>Email: {userData.email}</p>
+    </Jumbotron>
+    </Container>
   );
 }
 
