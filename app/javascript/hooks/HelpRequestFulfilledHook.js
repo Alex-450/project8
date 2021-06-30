b01557eac1004707dc40f5eb608078fc18cd3877
AlexStearn <53116798+Alex-450@users.noreply.github.com>
2021-06-30 17:14:50 +0200

    Add confirm for marking request as fulfilled

--- app/javascript/hooks/HelpRequestFulfilledHook.js
+++ app/javascript/hooks/HelpRequestFulfilledHook.js
@@ -1,25 +1,32 @@
 const HelpRequestFulfilled = (id) => {
-  const csrf = document
-    .querySelector("meta[name='csrf-token']")
-    .getAttribute("content");
+  if (
+    confirm(
+      "Are you sure you want to mark this request as completed? This cannot be undone"
+    )
+  ) {
+    const csrf = document
+      .querySelector("meta[name='csrf-token']")
+      .getAttribute("content");
 
-  fetch(`api/v1/help_requests/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "X-CSRF-Token": csrf,
-    },
-    body: JSON.stringify({
-      fulfilled: true,
-    }),
-  }).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Network response was not ok.");
-  });
-  window.location.reload(false);
+    fetch(`api/v1/help_requests/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "X-CSRF-Token": csrf,
+      },
+      body: JSON.stringify({
+        fulfilled: true,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response was not ok.");
+    });
+    window.location.reload(false);
+  } else {
+  }
 };
 
 export default HelpRequestFulfilled;
