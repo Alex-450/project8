3da3f3505d54d7acccee1c36031a4142744a5c13
AlexStearn <53116798+Alex-450@users.noreply.github.com>
2021-06-30 17:13:53 +0200

    Add withRouter to LandingPage

--- app/javascript/components/LandingPage.js
+++ app/javascript/components/LandingPage.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { Container, Jumbotron, Button, Row, Col } from "react-bootstrap";
+import { Container, Jumbotron, Button, Row } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
 
-const LandingPage = () => (
-  <div>
-    <Container fluid className="landing_container">
-      <Jumbotron className="landing_jumbotron">
-        <Row className="justify-content-center">
-          <h1 className="landing_header">Local aid</h1>
-        </Row>
-        <Row className="m-4">
-          <Button className="landing_button" href="/signup">
-            Sign up <span className="button_arrow">→</span>
-          </Button>
-        </Row>
-        <Row>
-          <Button className="landing_button" href="/login">
-            Log in <span className="button_arrow">→</span>
-          </Button>
-        </Row>
-      </Jumbotron>
-    </Container>
-  </div>
-);
+function LandingPage() {
+  return (
+    <div>
+      <Container fluid className="landing_container">
+        <Jumbotron className="landing_jumbotron">
+          <Row className="justify-content-center">
+            <h1 className="landing_header">Local aid</h1>
+          </Row>
+          <Row className="m-4">
+            <Button className="landing_button" href="/signup">
+              Sign up <span className="button_arrow">→</span>
+            </Button>
+          </Row>
+          <Row>
+            <Button className="landing_button" href="/login">
+              Log in <span className="button_arrow">→</span>
+            </Button>
+          </Row>
+        </Jumbotron>
+      </Container>
+    </div>
+  );
+}
 
-export default LandingPage;
+export default withRouter(LandingPage);
